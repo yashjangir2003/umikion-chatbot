95ad9b461d45f59b69dc6bbf90b67664cbcec639
Yash Jangir <yjyjyj6511@gmail.com>
2026-03-30 16:11:37 +0530

    fix deploy

--- app.py
+++ app.py
@@ -50,4 +50,7 @@ def chatbot_response():
     return jsonify({"response": get_response(user_input)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
